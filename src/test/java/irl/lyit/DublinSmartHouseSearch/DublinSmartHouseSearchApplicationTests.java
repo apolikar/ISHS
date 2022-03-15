e661e49d913b83a1d447a282acc62708685198f1
Aleksejs Polikarpovs <L00169705@student.lyit.ie>
2022-03-15 18:38:13 +0000

    create project init

---
+++ src/test/java/irl/lyit/DublinSmartHouseSearch/DublinSmartHouseSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package irl.lyit.DublinSmartHouseSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DublinSmartHouseSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
