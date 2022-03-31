b22d28d15c3c127857265086c183801e5a37b57a
Aleksejs Polikarpovs <L00169705@student.lyit.ie>
2022-03-31 18:25:54 +0100

    added wicket functionality, HomePage and HomePage.html

--- src/main/java/irl/lyit/DublinSmartHouseSearch/DublinSmartHouseSearchApplication.java
+++ src/main/java/irl/lyit/DublinSmartHouseSearch/DublinSmartHouseSearchApplication.java
@@ -1,22 +1,45 @@
 package irl.lyit.DublinSmartHouseSearch;
 
+import com.giffing.wicket.spring.boot.context.extensions.WicketApplicationInitConfiguration;
+import irl.lyit.DublinSmartHouseSearch.presentation.HomePage;
+import org.apache.wicket.Application;
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories(basePackages = "irl.lyit.DublinSmartHouseSearch.dao")
-public class DublinSmartHouseSearchApplication {
+public class DublinSmartHouseSearchApplication extends WebApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(DublinSmartHouseSearchApplication.class)
+                .run(args);
+    }
 
-		new SpringApplicationBuilder()
-				.sources(DublinSmartHouseSearchApplication.class)
-				.run(args);
+    @Autowired(required = false)
+    private List<WicketApplicationInitConfiguration> configurations = new ArrayList<>();
 
-	}
+    @Override
+    public Class<? extends Page> getHomePage() {
+        return HomePage.class;
+    }
 
+    @Override
+    protected void init() {
+        super.init();
+
+        for (WicketApplicationInitConfiguration configuration : configurations) {
+            configuration.init(this);
+        }
+    }
 
 }
--- src/main/java/irl/lyit/DublinSmartHouseSearch/presentation/HomePage.java
+++ src/main/java/irl/lyit/DublinSmartHouseSearch/presentation/HomePage.java
@@ -1,6 +1,69 @@
 package irl.lyit.DublinSmartHouseSearch.presentation;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import irl.lyit.DublinSmartHouseSearch.old.GeoCoordinates;
+import irl.lyit.DublinSmartHouseSearch.service.GeoCoordinatesFinder;
+import irl.lyit.DublinSmartHouseSearch.service.addressFormatter.GoogleAddressFormatter;
+import irl.lyit.DublinSmartHouseSearch.service.client.GMapsHTTPClient;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
+import java.io.IOException;
+
+
+@WicketHomePage
 public class HomePage extends WebPage {
+    public HomePage() {
+        add(new AddressForm("form"));
+
+    }
+
+    private static class AddressForm extends Form<Void> {
+
+        private final Model<String> addressModel;
+
+        public AddressForm(String id) {
+            super(id);
+
+            this.addressModel = new Model<>();
+
+
+            add(new Label("addressLabel", "address"));
+            add(new TextField<>("workInput", addressModel));
+
+
+        }
+
+
+        @Override
+        protected void onSubmit() {
+
+            // for test
+
+            GoogleAddressFormatter addressFormatter = new GoogleAddressFormatter();
+            String formattedAddress = addressFormatter.formatAddress(addressModel.getObject());
+
+            GMapsHTTPClient gMapsHTTPClient = new GMapsHTTPClient();
+            GeoCoordinates finalDestinationCoord;
+            try {
+
+                JsonNode addressResponse = gMapsHTTPClient.finalDestination(formattedAddress);
+                double lat = addressResponse.get("lat").asDouble();
+                double lng = addressResponse.get("lng").asDouble();
+                finalDestinationCoord = new GeoCoordinates(lat, lng);
+                System.out.println(finalDestinationCoord);
+
+            } catch (IOException | InterruptedException e) {
+                System.out.println("exception caught");
+            }
+
+
+            //System.out.println(addressModel.getObject());
+        }
+
+    }
 }
