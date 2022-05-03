26cc19d2ad99b079b5346158228a3a7ef2e48f1f
Aleksejs Polikarpovs <L00169705@student.lyit.ie>
2022-05-03 15:46:52 +0100

    added ResultMatchHouseConstructorTest

---
+++ src/test/java/irl/lyit/DublinSmartHouseSearch/service/timeMatrix/ResultMatchHouseConstructorTest.java
@@ -0,0 +1,21 @@
+package irl.lyit.DublinSmartHouseSearch.service.timeMatrix;
+
+import irl.lyit.DublinSmartHouseSearch.dao.House;
+import irl.lyit.DublinSmartHouseSearch.service.ResultMatchHouse;
+import junit.framework.TestCase;
+
+public class ResultMatchHouseConstructorTest extends TestCase {
+
+    public void testResultMatchHouseConstructor() {
+
+        ResultMatchHouse testObject = new ResultMatchHouse(
+                new House(),
+                1240
+        );
+
+        String expectedResult = "20:40";
+        String actualResult = testObject.getTimeString();
+
+        assertEquals(expectedResult, actualResult);
+    }
+}
