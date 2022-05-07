12980d297e2bb8cc3b423070d848a39e9d20e07f
Aleksejs Polikarpovs <L00169705@student.lyit.ie>
2022-05-07 18:24:02 +0100

    added jaxb-api into the pom.xml file

--- src/test/java/irl/lyit/DublinSmartHouseSearch/old/SearchAttributesTest.java
+++ src/test/java/irl/lyit/DublinSmartHouseSearch/old/SearchAttributesTest.java
@@ -11,22 +11,22 @@ public class SearchAttributesTest extends TestCase {
     public void testSearchAttributesConstructor() throws IOException, InterruptedException {
 
 
-//        SearchAttributes searchAttributes = new SearchAttributes(
-//            new GeoCoordinates(52.1383154, -8.6603111),
-//                "2022-05-01T08:00:00.000Z",
-//                1800,
-//                "public_transport",
-//                5,
-//                10,
-//                1,
-//                3
-//        );
-//
-//        assertEquals("Time Limit Test", 1800, searchAttributes.getTimeLimit());
-//        // finish all fields
-//
-//
-//
+        SearchAttributes searchAttributes = new SearchAttributes(
+            new GeoCoordinates(52.1383154, -8.6603111),
+                "2022-05-01T08:00:00.000Z",
+                1800,
+                "public_transport",
+                5,
+                10,
+                1,
+                3
+        );
+
+        assertEquals("Time Limit Test", 1800, searchAttributes.getTimeLimit());
+        // finish all fields
+
+
+
 
     }
 }
